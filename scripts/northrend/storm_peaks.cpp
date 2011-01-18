2539d27e897822933477946063d6c8b88f66e6bd
GMHAZE <iceis2@hotmail.com>
2011-01-18 16:39:05 -0500

    Signed-off-by: GMHAZE <iceis2@hotmail.com>

--- scripts/northrend/storm_peaks.cpp
+++ scripts/northrend/storm_peaks.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2006 - 2010 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+/* Copyright (C) 2006 - 2011 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -272,4 +272,5 @@ void AddSC_storm_peaks()
     newscript->pGossipHello = &GossipHello_npc_roxi_ramrocket;
     newscript->pGossipSelect = &GossipSelect_npc_roxi_ramrocket;
     newscript->RegisterSelf();
+
 }
